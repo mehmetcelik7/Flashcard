7d26a7420273df8a7ff6d3a8c47844efd475ea1d
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-22 20:38:06 +0300

    FillBarView

---
+++ Flashcard/View/FillBarView.swift
@@ -0,0 +1,18 @@
+//
+//  FillBarView.swift
+//  Flashcard
+//
+//  Created by mehmet Çelik on 22.03.2025.
+//
+
+import SwiftUI
+
+struct FillBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FillBarView()
+}
