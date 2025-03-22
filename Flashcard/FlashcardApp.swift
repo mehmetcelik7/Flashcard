0ec2e0f4a07329e2c4f6db2881afebea1deec632
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-22 18:02:28 +0300

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by mehmet Çelik on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
