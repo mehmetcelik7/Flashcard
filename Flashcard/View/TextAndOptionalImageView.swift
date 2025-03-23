106b31106634638a6e5879dc77c0344651c0410e
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-23 12:40:13 +0300

    Text and Image

---
+++ Flashcard/View/TextAndOptionalImageView.swift
@@ -0,0 +1,18 @@
+//
+//  TextAndOptionalImageView.swift
+//  Flashcard
+//
+//  Created by mehmet Çelik on 23.03.2025.
+//
+
+import SwiftUI
+
+struct TextAndOptionalImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAndOptionalImageView()
+}
